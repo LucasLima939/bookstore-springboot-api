1375d2173d8f2b791fde174f6b17bd54e9dc1e61
lucas lima <lucaslima939@gmail.com>
2021-04-02 01:10:13 -0300

    password encoder

--- src/main/java/aplicacao/AplicacaoSpring.java
+++ src/main/java/aplicacao/AplicacaoSpring.java
@@ -24,15 +24,15 @@ public class AplicacaoSpring {
     public CommandLineRunner run(AplicacaoSimplesSpring application) throws Exception {
         return args -> {        	
         	
-        	application.cadastrarUsuario(new Cadastro(
-    				"Time04", // NOME
-    				"1111111112", //CPF
-    				"example@example.com.br", //EMAIL
-    				"time04", // LOGIN
-    				"01001111 01001000"), //SENHA
-    				"50771-140", // CEP
-    				"680" // NUMERO
-        			);
+//        	application.cadastrarUsuario(new Cadastro(
+//    				"Time04", // NOME
+//    				"1111111112", //CPF
+//    				"example@example.com.br", //EMAIL
+//    				"time04", // LOGIN
+//    				"01001111 01001000"), //SENHA
+//    				"50771-140", // CEP
+//    				"680" // NUMERO
+//        			);
         	
         };
     }
--- src/main/java/aplicacao/resource/LoginResource.java
+++ src/main/java/aplicacao/resource/LoginResource.java
@@ -1,19 +1,25 @@
 package aplicacao.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aplicacao.model.Login;
+import aplicacao.model.Sessao;
+import aplicacao.service.LoginService;
 
 @RestController
 @RequestMapping("/login")
 public class LoginResource {
+	@Autowired
+	private LoginService loginService;
 	
 	@PostMapping
-	public void login(@RequestBody Login login) {
-		
+	public Sessao login(@RequestBody Login login) {
+		return loginService.logar(login);
+			
 	}
 
 }
